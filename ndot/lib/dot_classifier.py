d3a34d8045dc395a2c128790fea962a2ff27469f
Sandesh Kalantre <kalantresandesh@gmail.com>
2017-05-25 17:07:39 -0400

    fixed a bug in gitignore, files added from last 2 commits

---
+++ ndot/lib/dot_classifier.py
@@ -0,0 +1,28 @@
+# Module to classify a grid x given a potential profile into dots, barriers and leads
+import numpy as np
+
+def get_mask(x,V):
+    '''
+    Input:
+        x : 1D grid
+        V : potential profile
+    Output:
+        mask : array of x.size with each element one of 'l1','b','d','l2' standing for lead1, barrier,dot and lead2 respectively
+    '''
+    # CHEAP TESTING SOLUTION
+    mask = np.array(['l1','d','l2'])
+    return mask
+
+def get_dot_info(mask):
+    ''' 
+    Input:
+        mask : mask as described in the get_mask function
+    Output:
+        dot_info : (Dictonary) key = dot_number, value = [dot_begin, dot_end]
+    '''
+    # CHEAP TESTING SOLUTION
+    dot_info = {0 : [1,1]}
+    return dot_info
+    
+
+
---
+++ ndot/lib/thomas_fermi.py
@@ -0,0 +1,142 @@
+# Module to solve the Thomas-Fermi problem in 1D
+
+import numpy as np
+import dot_classifier
+
+def create_K_matrix(x, E_scale=1.0, sigma=1.0):
+    '''
+    Input: 
+        x : discrete 1D grid
+        E_scale : energy scale for the K matrix, default units is eV
+        sigma : impact paramter to prevent blow up at the same point
+    Output:
+        K : matrix of size x.size times x.size
+
+    K(x1,x2) = E_scale / sqrt((x1 - x2)^2 + sigma^2)
+    '''
+    K = E_scale/np.sqrt((x[:, np.newaxis] - x)**2 + sigma**2)
+    return K
+
+def solve_thomas_fermi(x,V,K,mu_l,N_dot):
+    '''
+    Input:
+        x    : discrete 1D grid
+        V    : potential
+        K    : Coulomb interaction matrix between two points
+        mu_l : (mu_L1, mu_L2) tuple with the lead potentials
+        N_dot: vector with number of electrons in each dot, can be of size 0 i.e no dot
+    Output:
+        (n, mu) where
+        n    : electronic charge density as a function of x
+        mu   : chemical potential as a function of x
+               mu(x) = mu_L when x in leads
+    
+    Solves the Thomas-Fermi equation V - mu + K n = 0 along with the constraint that integral of electron density in a dot is a constant and electron density in the barrier region is zero.
+    '''
+    #solve the equation A z = b
+    # z = (n mu)^T
+    #set up the A matrix
+    N_points = x.size
+    A = np.zeros([2*N_points,2*N_points])
+
+    mask = dot_classifier.get_mask(x,V)
+    # dictionary index by dot number, gives [dot_begin_index,dot_end_index]
+    dot_info = dot_classifier.get_dot_info(mask)
+
+
+    # top left half for n
+    for i in range(N_points):
+        for j in range(N_points):
+            A[i,j]  = K[i,j]
+    # top right half for the chemical potentials
+    for i in range(N_points):
+        A[i,i + N_points] = -1
+
+    # for leads and barrier
+    # add the N_points number of constraints, index_constraint keeps track of number of constraint added
+    index_constraint = 0
+    index_dot = -1
+    while (index_constraint < N_points):
+        if (mask[index_constraint] == 'l1' or mask[index_constraint] == 'l2'):
+            A[index_constraint + N_points,index_constraint + N_points] = 1
+            index_constraint += 1
+        elif(mask[index_constraint] == 'b'):
+            A[index_constraint+N_points,index_constraint] = 1
+            index_constraint += 1
+        else:
+            # mask is a dot
+            index_dot += 1
+            value = dot_info[index_dot]
+            dot_begin = value[0]
+            dot_end = value[1]
+            dot_size = dot_end - dot_begin + 1
+            for j in range(dot_size):
+                A[index_constraint + N_points, dot_begin + j] = 1   
+            # now the number of electrons in a dot is fixed constraint has been added
+            index_constraint += 1
+
+            # add the mu is same over the dot constraint
+            # note that there are only dot_size - 1 constraints of this type
+            for j in range(dot_size-1): 
+                A[index_constraint + N_points, index_constraint + N_points - 1] = 1 
+                A[index_constraint + N_points, index_constraint + N_points] = -1 
+                index_constraint += 1
+
+    # set up the RHS
+    b = np.zeros(2*N_points)
+    for i in range(N_points):
+        b[i] = -V[i]
+
+    # lead constraints
+    index_constraint = 0
+    index_dot = -1
+    while (index_constraint < N_points):
+        if (mask[index_constraint] == 'l1'): 
+            b[index_constraint + N_points] = mu_l[0]
+            index_constraint += 1
+        elif (mask[index_constraint] == 'l2'):
+            b[index_constraint + N_points] =  mu_l[1]
+            index_constraint += 1
+        elif (mask[index_constraint] == 'b'):
+            # actually do nothing since b is already 0
+            b[index_constraint + N_points] = 0
+        else:
+            index_dot += 1
+            value = dot_info[index_dot]
+            dot_begin = value[0]
+            dot_end = value[1]
+            dot_size = dot_end - dot_begin + 1
+            for j in range(dot_size):
+                b[index_constraint + N_points] = N_dot[index_dot]   
+            # now the number of electrons in a dot is fixed constraint has been added
+            index_constraint += 1
+
+            # add the mu is same over the dot constraint
+            # note that there are only dot_size - 1 constraints of this type
+            for j in range(dot_size-1): 
+                # again, do nothing since b is already 0
+                b[index_constraint + N_points] = 0
+                index_constraint += 1
+
+    print A,b
+    z = np.linalg.solve(A,b)
+    # return n,mu
+    return z[:N_points],z[N_points:]
+     
+def calculate_thomas_fermi_energy(V,K,n,mu):
+    '''
+    Input: 
+        V : potential profile
+        K : Coulomb interaction matrix 
+        n : electorn density
+        mu : chemical potenial profile, includes leads and barriers as well
+    Output:
+        E : Thomas-Fermi energy
+
+    E = V n + 1/2 n K n
+    '''
+    E = np.sum(V*n) + 0.5 * np.sum(n*np.dot(K,n))
+    return E
+
+
+    
